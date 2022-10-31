ae4881114b06382204b460b0d427cd0bfb3e66d9
FuzzySuki <115906123+FuzzySuki@users.noreply.github.com>
2022-10-31 17:25:13 +0100

    Add files via upload

--- noclip.lua
+++ noclip.lua
@@ -1,17 +1,33 @@
-noclip = false
-game:GetService('RunService').Stepped:connect(function()
-if noclip then
-game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-end
-end)
-plr = game.Players.LocalPlayer
-mouse = plr:GetMouse()
-mouse.KeyDown:connect(function(key)
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+local runservice = game:GetService("RunService")
+local noclip = false
  
-if key == "j" then
-noclip = not noclip
-game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-end
+local msg = Instance.new("Message", player.PlayerGui)
+msg.Text = "Noclip Script by TheBaconHairdhaxerz Press on 'e' to noclip & 't' to destroy this message!"
+ 
+runservice.Stepped:Connect(function()
+    if noclip then
+        player.Character.Humanoid:ChangeState(11)
+    end
 end)
-print('Noclip Loaded!')
-print('Press "J" to noclip')
+ 
+mouse.KeyDown:Connect(function(key)
+    if key == "t" then
+        msg:Destroy()
+    end
+end)
+ 
+mouse.KeyDown:Connect(function(key)
+    if key == "m" then
+        noclip = true
+        player.Character.Humanoid:ChangeState(11)
+    end
+end)
+
+mouse.KeyDown:Connect(function(key)
+    if key == "z" then
+        noclip = false
+        player.Character.Humanoid:ChangeState(11)
+    end
+end)
